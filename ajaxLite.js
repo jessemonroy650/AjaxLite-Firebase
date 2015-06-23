c95ade3ae885fdfdf410d966a3ceb74551c435f1
Jesse Monroy <jessemonroy650@yahoo.com>
2015-06-23 03:27:21 -0700

    exchange tabs for spaces

--- ajaxLite.js
+++ ajaxLite.js
@@ -5,85 +5,85 @@
 // For JSON, see http://json.org/
 var AJAX = {
 
-	xhttp   : null,
-	// list of pairs as need, all expected to be in the HTTP protocol
-	// SEE: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
-	headers : [{'' : ''}],
+    xhttp   : null,
+    // list of pairs as need, all expected to be in the HTTP protocol
+    // SEE: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
+    headers : [{'' : ''}],
 
-	init : function (callback, headers) {
-		// It's not clear this will work, so it's not implemented:
-		// https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest#XMLHttpRequest%28%29
-		// https://bugzilla.mozilla.org/show_bug.cgi?id=692677#c68
-		xhttp = new XMLHttpRequest();
-		if (typeof callback == 'function') {
-			xhttp.onreadystatechange = function() {
-				if (xhttp.readyState === 4 && xhttp.status === 200) {
-					callback(xhttp.responseText);
-				} else {
-					// https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest#getAllResponseHeaders%28%29
-					//alert(xhttp.getAllResponseHeaders());
-				}
-			}
-		} else if (typeof callback == 'string') {
-			xhttp.onreadystatechange = function() {
-				if (xhttp.readyState === 4 && xhttp.status === 200) {
-					document.getElementById(callback).innerHTML = xhttp.responseText;
-				} else {
-					//alert(xhttp.getAllResponseHeaders());
-				}
-			}
-		} else {
-			alert('AJAX.init, No tag or callback');
-		}
-	},
-	// https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
-	// On ''async''
-	// An optional boolean parameter, defaulting to true, indicating whether or
-	// not to perform the operation asynchronously.
-	// If this value is false, the send()method does not return until
-	// the response is received.  If true, notification of a completed transaction
-	// is provided using event listeners. This must be true if the multipart
-	// attribute is true, or an exception will be thrown. 
-	GET : function (URL) {
-		async = true; // This is explicit for clarity.
-		xhttp.open('GET', URL, async);
-		xhttp.send();
-		// After this the callback handles the returning data.
-	},
-	//
-	POST : function (URL, data) {
-		async = true; // This is explicit for clarity.
-		xhttp.open("POST", URL, true);
-		if ( this.headers.length > 0 ) {
-			//xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-		}
-		xhttp.send(data);
-	},
-	//
-	PUT : function (URL, data) {
-		async = true; // This is explicit for clarity.
-		xhttp.open("PUT", URL, true);
-		if ( this.headers.length > 0 ) {
-			//xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-		}
-		xhttp.send(data);
-	},
-	//
-	PATCH : function (URL, data) {
-		async = true; // This is explicit for clarity.
-		xhttp.open("PATCH", URL, true);
-		if ( this.headers.length > 0 ) {
-			//xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-		}
-		xhttp.send(data);
-	},
-	//
-	DELETE : function (URL) {
-		async = true; // This is explicit for clarity.
-		xhttp.open("DELETE", URL, true);
-		if ( this.headers.length > 0 ) {
-			//xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-		}
-		xhttp.send();
-	}
+    init : function (callback, headers) {
+        // It's not clear this will work, so it's not implemented:
+        // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest#XMLHttpRequest%28%29
+        // https://bugzilla.mozilla.org/show_bug.cgi?id=692677#c68
+        xhttp = new XMLHttpRequest();
+        if (typeof callback == 'function') {
+            xhttp.onreadystatechange = function() {
+                if (xhttp.readyState === 4 && xhttp.status === 200) {
+                    callback(xhttp.responseText);
+                } else {
+                    // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest#getAllResponseHeaders%28%29
+                    //alert(xhttp.getAllResponseHeaders());
+                }
+            }
+        } else if (typeof callback == 'string') {
+            xhttp.onreadystatechange = function() {
+                if (xhttp.readyState === 4 && xhttp.status === 200) {
+                    document.getElementById(callback).innerHTML = xhttp.responseText;
+                } else {
+                    //alert(xhttp.getAllResponseHeaders());
+                }
+            }
+        } else {
+            alert('AJAX.init, No tag or callback');
+        }
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+    // On ''async''
+    // An optional boolean parameter, defaulting to true, indicating whether or
+    // not to perform the operation asynchronously.
+    // If this value is false, the send()method does not return until
+    // the response is received.  If true, notification of a completed transaction
+    // is provided using event listeners. This must be true if the multipart
+    // attribute is true, or an exception will be thrown. 
+    GET : function (URL) {
+        async = true; // This is explicit for clarity.
+        xhttp.open('GET', URL, async);
+        xhttp.send();
+        // After this the callback handles the returning data.
+    },
+    //
+    POST : function (URL, data) {
+        async = true; // This is explicit for clarity.
+        xhttp.open("POST", URL, true);
+        if ( this.headers.length > 0 ) {
+            //xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        }
+        xhttp.send(data);
+    },
+    //
+    PUT : function (URL, data) {
+        async = true; // This is explicit for clarity.
+        xhttp.open("PUT", URL, true);
+        if ( this.headers.length > 0 ) {
+            //xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        }
+        xhttp.send(data);
+    },
+    //
+    PATCH : function (URL, data) {
+        async = true; // This is explicit for clarity.
+        xhttp.open("PATCH", URL, true);
+        if ( this.headers.length > 0 ) {
+            //xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        }
+        xhttp.send(data);
+    },
+    //
+    DELETE : function (URL) {
+        async = true; // This is explicit for clarity.
+        xhttp.open("DELETE", URL, true);
+        if ( this.headers.length > 0 ) {
+            //xhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        }
+        xhttp.send();
+    }
 }
